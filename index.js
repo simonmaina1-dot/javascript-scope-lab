92a176b603fcb450f86866e9f98c3d2fa6ec4fc4
sanchez@GitauJW <simon.maina1@student.moringaschool.com>
2025-11-29 22:14:49 +0300

    changed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// Global Scope
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function Scope
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Block Scope
+if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Update Global Variable
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+module.exports = {
+    burgers,
+    featuredDrink,
+    addBurger,
+    changeFeaturedDrink
+};
